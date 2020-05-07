32a9058a166ba203ae445f246243182018e0afd9
obfrap <masterofdestruction@gmail.com>
2020-05-07 14:53:22 -0700

    Have completed part one pulling cleaning and analyzing data from OpenWeathermap.org.  compared temp, hum, wind spedd and cloudiness to latitude for randomly generated list of cities

--- Weather Py/api_keys.py
+++ Weather Py/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7c8ba3b89e221dc4d574379d38afb79c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDj3eYoBr5rmbJLYqotWo3rakoYqz5RpB8"
